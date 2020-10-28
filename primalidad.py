3102fc796489342604c250c9f272874a4a1343a9
Marly Mejia <marly.mejia@gmail.com>
2020-10-28 22:20:20 +0100

    clase 26: prueba de primalidad

---
+++ primalidad.py
@@ -0,0 +1,24 @@
+def es_primo(n):
+    contador = 0
+
+    for i in range(1, n + 1):
+        if i == 1 or i == n:
+            continue
+        if n %i == 0:
+            contador += 1
+    if contador == 0:
+        return True
+    else:
+        return False
+
+
+def run():
+    numero = int(input('Escribe un número: '))
+    if es_primo(numero):
+        print('Es primo')
+    else:
+        print('No es primo')
+
+
+if __name__ == "__main__":
+    run()
