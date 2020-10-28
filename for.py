c3198789d557eaaae9a9d946e011821ddd8139c9
Marly Mejia <marly.mejia@gmail.com>
2020-10-28 17:15:39 +0100

    clase 24: Recorriendo un string con for

---
+++ for.py
@@ -0,0 +1,15 @@
+def run():
+    # for i in range(1000):
+    #     print(i)
+    
+    # nombre = input('Escribe tu nombre: ')
+    # for i in nombre:
+    #     print(i)
+
+    frase = input('Escribe una frase: ')
+    for letra in frase:
+        print(letra.upper())
+
+
+if __name__ == "__main__":
+    run()
